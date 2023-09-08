dd3863dec0e68e1d38b75fa443eb77bcb34d8fb7
djblackett <49618491+djblackett@users.noreply.github.com>
2023-09-08 01:49:01 -0300

    fix cypress port

--- bloglist-frontend/cypress/e2e/blog_frontend.cy.js
+++ bloglist-frontend/cypress/e2e/blog_frontend.cy.js
@@ -2,14 +2,14 @@
 
 describe("Blog app", function() {
   beforeEach(function() {
-    cy.request("POST", "http://localhost:3000/api/testing/reset");
+    cy.request("POST", "http://0.0.0.0:3000/api/testing/reset");
     const user = {
       name: "Doctor Testerman",
       username: "testU",
       password: "testP"
     };
-    cy.request("POST", "http://localhost:3000/api/users/", user);
-    cy.visit("http://localhost:3000");
+    cy.request("POST", "http://0.0.0.0:3000/api/users/", user);
+    cy.visit("http://0.0.0.0:3000");
   });
 
 
@@ -100,7 +100,7 @@ describe("Blog app", function() {
         password: "testP2"
       };
 
-      cy.request("POST", "http://localhost:3000/api/users/", user);
+      cy.request("POST", "http://0.0.0.0:3000/api/users/", user);
 
       cy.get("#logout-button").click();
 
